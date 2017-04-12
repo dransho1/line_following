cace0a728bb2ed45910481b8ea9ed3774cba7611
David Ranshous <david.ranshous@gmail.com>
2017-04-11 22:27:25 -0400

    simple avoidance

--- scripts/line_follower.py
+++ scripts/line_follower.py
@@ -5,12 +5,14 @@ import rospy
 import random
 import servotest as vc
 import math
+import numpy as np
 
 #from geometry_msgs.msg import Twist
 #from kobuki_msgs.msg import SensorState
 from std_msgs.msg import Float64, Int32, Int32MultiArray
 from joy_test.msg import IntList
 from blobfinder.msg import MultiBlobInfo
+from sensor_msgs.msg import LaserScan
 
 # control at 10Hz
 CONTROL_PERIOD = rospy.Duration(0.1)
@@ -28,7 +30,7 @@ WAIT_DURATION = rospy.Duration(1.0)
 RANDOM_ACTION_DURATION = rospy.Duration(0.5)
 
 #global controller variables
-MOTOR_NEUTRAL = 1500
+MOTOR_NEUTRAL = 1500    # neutral is 0 from controller to +/-90
 ESC_SERVO = 1
 STEER_SERVO = 0
 STEER_NEUTRAL = 90
@@ -58,8 +60,13 @@ class Controller:
         # set up a killswitch
         self.kill = 0
 
-        # set up a searching cue
-        self.search = 0
+        # turning cue
+        self.turn = 0
+        self.avoid = 0
+        self.avoid_angle = 0
+
+        # subscrib to laserscan messages
+        rospy.Subscriber('scan', LaserScan, self.laser_callback)
 
         # pick out a random action to do when we start driving
         #self.start_straight()
@@ -94,7 +101,28 @@ class Controller:
         #else:
             #self.controller.setAngle(STEER_SERVO, steering)
             #self.controller.setPosition(ESC_SERVO, MOTOR_NEUTRAL + 2*throttle)
-    
+
+    def laser_callback(self, scan):
+        ranges = scan.ranges
+        max_range = np.nanmax(ranges)
+        min_range = np.nanmin(ranges)
+        index_min = np.nanargmin(ranges)
+        angle_min = scan.angle_min
+        angle_max = scan.angle_max
+        angle_inc = scan.angle_increment
+        #rospy.loginfo('max range is: %f', max_range)
+        rospy.loginfo('min range is: %f', min_range)
+        # simple avoidance won't work because blob callback is too frequent
+        # need to prioritize avoidance
+        if (min_range < 0.5):
+            self.avoid = 1
+            angle = math.degrees(angle_min + (angle_inc * index_min))
+            # 28 to -28, 0 mid
+            angle = int((angle*3)+90) # convert to 180 to 0 scale, 90 mid
+            self.avoid_angle = angle
+        else:
+            self.avoid = 0
+            
     def blob_callback(self, data):
         num = len(data.blobs)
         rospy.loginfo('got a message with %d blobs', num)
@@ -116,13 +144,14 @@ class Controller:
             max_y.append(data.blobs[i].cy)
             maxY = max(max_y)
 
-        # when no blobs appear, enter look_for_line callback
-        # should wait certain duration before calling Timer, and after
-        # waiting, then it can check if number of blobs are still zero 
-        
+        # when no blobs appear, enter search state        
         if (num == 0) or (maxBlob < 60) or (maxY < int(screen_width/3)):
             self.state = 'search'
             rospy.loginfo('searching')
+        elif (self.avoid == 1):
+            self.state = 'avoid'
+            rospy.loginfo('set avoid state')
+            #self.controller.setAngle(STEER_SERVO, self.avoid_angle) 
         else:
             self.state = 'following'
             # with maxblob, now calculate direction
@@ -135,6 +164,8 @@ class Controller:
             steering = blob_ratio*steer_range
             steering = steer_range - steering
             steering = int(math.ceil(steering))
+            if abs(steering - STEER_NEUTRAL) > 25:
+                self.turn = 1
             self.controller.setAngle(STEER_SERVO, steering) 
             rospy.loginfo('blob number: %d',numBlob)
             rospy.loginfo('blob x coordinate: %d',data.blobs[numBlob].cx)
@@ -165,17 +196,25 @@ class Controller:
         elif self.state == 'search':
             rospy.loginfo('control search')
             search_angle = 0
-            search_thr = 40
+            search_thr = 50
             self.controller.setAngle(STEER_SERVO, search_angle)
             self.controller.setPosition(ESC_SERVO, MOTOR_NEUTRAL
                                         + int(math.ceil(1.5*search_thr)))
         elif self.state == 'following':
             rospy.loginfo('state: following')
-            thr = 45
+            if self.turn == 1:
+                thr = 50
+            else:
+                thr = 45
             self.controller.setPosition(ESC_SERVO, MOTOR_NEUTRAL +
                                         int(math.ceil(1.5*thr)))
             rospy.loginfo('throttle is: %d',thr)
-
+        elif self.state == 'avoid':
+            rospy.loginfo('avoidance activated')
+            thr = 50
+            self.controller.setAngle(STEER_SERVO, STEER_NEUTRAL)
+            self.controller.setPosition(ESC_SERVO, MOTOR_NEUTRAL)
+                                        #+ int(math.ceil(1.5*thr)))
         
     # called by main function below (after init)
     def run(self):
